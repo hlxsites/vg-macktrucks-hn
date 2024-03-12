cf8cfa0e5da8b227762d8765085048372298fa89
Jonatan Lledo <117984204+jonatan-lledo-netcentric@users.noreply.github.com>
2024-03-12 10:19:17 +0100

    Add a constants file & remove the null in footer form

---
+++ scripts/constants.js
@@ -0,0 +1,15 @@
+// check if an active campaign is running or OneTrust needs scan the active scripts
+export const COOKIE_CHECK = true;
+
+// ONE TRUST COOKIE CONSENT
+export const DATA_DOMAIN_SCRIPT = '18961cc8-1604-4bea-8e69-a5e7879e56c5';
+
+// GOOGLE TAG MANAGER ID
+export const GTM_ID = 'GTM-5DKKVHFL';
+
+// COOKIE Constants
+export const COOKIE_VALUES = {
+  performance: 'C0002',
+  targeting: 'C0004',
+  social: 'C0005',
+};
